1a22911fe796c299f6ad5c3aefc870c7425e3436
ROBO-LAPTOP\Raz <Raz@yosigal.com>
2017-12-03 15:35:27 +0200

    Clean checkout from the Moodle submission of project 07

--- CodeWriter.py
+++ CodeWriter.py
@@ -294,7 +294,7 @@ class CodeWriter:
         # Write comment in output
         self.writeComment("__saveValueInTemp")
 
-        safe_value = str(value)
+        safe_value = value
         self.__writeLine(LOAD_A + safe_value)
         self.__writeLine(D_REG + ASSIGN + A_REG)
         self.__writeLine(LOAD_A + TEMP_0)
